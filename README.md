d83a96a5e6f96394be075c928d15c8e14cd4add3
Avery Whitehead <averywhthead@gmail.com>
2025-02-23 15:39:35 -0800

    Add backtracking_coins.py

---
+++ README.md
@@ -0,0 +1,5 @@
+Bits of code for reference, learning & practice
+
+### backtracking_coins
+
+Use of a backtracking algorithm to calcukate how many differents ways £2 can be made using any denominations of coins
---
+++ backtracking_coins.py
@@ -0,0 +1,20 @@
+def solver(coins: list[int], total, chosen_coins: list[int]) -> list[list[int]]:
+    if total == 0:
+        return [chosen_coins]
+    if total < 0 or not coins:
+        return []
+
+    pick = coins[0]
+    use = solver(coins, total - pick, chosen_coins + [pick])
+    notuse = solver(coins[1:], total, chosen_coins)
+
+    if use is not None and notuse is not None:
+        return use + notuse
+    if use is not None:
+        return use
+    if notuse is not None:
+        return notuse
+    return []
+
+
+print(len(solver([1, 2, 5, 10, 20, 50, 100, 200], 200, [])))
